8fa612a602212f107dc0a7373b8179bd1f2f979c
Felipe Amorim <felipe21fab@gmail.com>
2023-04-03 19:52:25 -0300

    Created task object

--- src/task.js
+++ src/task.js
@@ -1,3 +1,12 @@
-const TODO = function(title, description, dueDate, priority){
+
+const toDo = (title, description, dueDate, priority) => {
+    const getTitle = () => title
+    const getDescription = () => description
+    const getDueDate = () => dueDate
+    const getPriority = () => priority
+
+    return {getTitle, getDescription, getDueDate, getPriority}
     
 }
+
+export default toDo
