fd18a3672d5977a0aa4c4bc598157121cad5e427
Felipe Amorim <felipe21fab@gmail.com>
2023-04-24 19:48:48 -0300

    Finished taskForm

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 import toDo from "./toDo"
 import MakeToDOCard from "./MakeToDoCard"
+import taskform from "./taskForm"
+import project from "./project"
 
-const contentContainer = document.getElementById('content')
+const content = document.getElementById('content')
+const left = document.createElement('div')
+const right = document.createElement('div')
 
-/*
-contentContainer.innerHTML = page()
-*/
+const h1 = document.createElement('h1')
+h1.innerHTML = 'To Do List'
 
-const todo1 = toDo('Lista', 'ashdhshdshd', '21/07/2014', 3)
-const div = MakeToDOCard(todo1)
+const projectsLabel = document.createElement('label')
+projectsLabel.htmlFor = 'projects'
+projectsLabel.innerHTML = 'Projects'
 
-contentContainer.innerHTML = div.outerHTML
+const projectsList = document.createElement('div')
+
+
+const projectsInput = document.createElement('input')
+projectsInput.type = 'text'
+projectsInput.name = 'project_name'
+projectsInput.id = 'projects'
+
+const projectsButton = document.createElement('button')
+projectsButton.id = 'projectBtn'
+projectsButton.innerHTML = 'Add Project'
+
+projectsButton.addEventListener('click', () => {
+    project(projectsInput.value)
+})
+
+const h2 = document.createElement('h2')
+h2.innerHTML = 'Tasks'
+
+left.appendChild(h1)
+left.appendChild(projectsLabel)
+left.appendChild(projectsInput)
+left.appendChild(projectsButton)
+
+
+
+content.appendChild(left)
