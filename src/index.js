de8955e90bd9c48931f379ab35e64aec56ac4943
Felipe Amorim <felipe21fab@gmail.com>
2023-04-29 11:52:47 -0300

    Add css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import './style.css'
+
 import { toDo } from "./toDo"
 import {project, renderProjects} from "./project"
 import projectsArray from "./projectArray"
+
+
 
 const content = document.getElementById('content')
 
@@ -16,10 +20,13 @@ projectsDIv.id = 'projectsdiv'
 const left = document.createElement('div')
 const right = document.createElement('div')
 
+left.id = 'left'
+
 const h1 = document.createElement('h1')
 h1.innerHTML = 'To Do List'
 
 const projectsLabel = document.createElement('label')
+projectsLabel.id = 'projectslabel'
 projectsLabel.htmlFor = 'projects'
 projectsLabel.innerHTML = 'Projects'
 
@@ -42,13 +49,13 @@ projectsButton.addEventListener('click', () => {
 })
 
 
-const objectButton = document.createElement('button')
+/* const objectButton = document.createElement('button')
 objectButton.innerHTML = 'Obejct'
 objectButton.addEventListener('click', () => {
     projectsArray.forEach((element) => {
         console.log(`${element.getName()} S: ${element.selected} todos: ${element.getToDos()}`)
     })
-})
+}) */
 
 
 
@@ -60,7 +67,6 @@ left.appendChild(projectsLabel)
 left.appendChild(projectsList)
 left.appendChild(projectsInput)
 left.appendChild(projectsButton)
-left.appendChild(objectButton)
 
 const todoBtnDiv = document.createElement('div')
 todoBtnDiv.id = 'todobtndiv'
